c472b1ced547f72f66071c72ac788e6eeb68becc
miragshin <miragshin@macbook.default>
2015-07-06 23:30:27 +0100

    Xakep

---
+++ hacker.py
@@ -0,0 +1,29 @@
+import os
+# Generating Links Magazine before 2013
+
+f = open('out.txt', 'w')
+
+def generate():
+    list = []
+    y=1999
+    while y <= 2012:
+        for i in range(1,13):
+            list.append("https://xakep.ru/wp-content/uploads/2014/08/xa_%02d_%d_low.pdf" %(i,y))
+        y = y +1
+# Generating links after 2013
+    z=2013
+    while z <= 2015:
+        for x in range(1,6):
+            list.append("https://xakep.ru/wp-content/uploads/2015/06/xakep_%02d_%d.pdf" %(x,z))
+        z = z + 1
+    for url in list:
+        print >>f, url
+    f.close()
+def command():
+    os.system('wget -i out.txt')
+
+def main():
+    generate()
+    command()
+
+main()
